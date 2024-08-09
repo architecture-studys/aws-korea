2ecf8d607031eceebb0ee1d7a26f629a0e6301e6
ondacloud <junseong8299@gmail.com>
2024-08-09 21:01:55 +0900

    aws-korea

--- Day2/01. 12) CICD - 광주/infra/00-0 file.tf
+++ Day2/01. 12) CICD - 광주/infra/00-0 file.tf
@@ -19,49 +19,49 @@ resource "aws_s3_bucket" "gwangju-cicd-manifest" {
 resource "aws_s3_object" "gwangju-cicd-buildspec" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/buildspec.yaml"
-  source = "./src/gwangju-cicd/buildspec.yaml"
-  etag   = filemd5("./src/gwangju-cicd/buildspec.yaml")
+  source = "./src/buildspec.yaml"
+  etag   = filemd5("./src/buildspec.yaml")
   content_type = "application/vnd.yaml"
 }
 
 resource "aws_s3_object" "gwangju-cicd-Docker" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/Dockerfile"
-  source = "./src/gwangju-cicd/Dockerfile"
-  etag   = filemd5("./src/gwangju-cicd/Dockerfile")
+  source = "./src/Dockerfile"
+  etag   = filemd5("./src/Dockerfile")
 }
 
 resource "aws_s3_object" "gwangju-cicd-app" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/main.py"
-  source = "./src/gwangju-cicd/main.py"
-  etag   = filemd5("./src/gwangju-cicd/main.py")
+  source = "./src/main.py"
+  etag   = filemd5("./src/main.py")
 }
 
 resource "aws_s3_object" "gwangju-cicd-deployment" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/deployment.yaml"
-  source = "./src/gwangju-cicd/deployment.yaml"
-  etag   = filemd5("./src/gwangju-cicd/deployment.yaml")
+  source = "./src/deployment.yaml"
+  etag   = filemd5("./src/deployment.yaml")
 }
 
 resource "aws_s3_object" "gwangju-cicd-kustomization" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/kustomization.yaml"
-  source = "./src/gwangju-cicd/kustomization.yaml"
-  etag   = filemd5("./src/gwangju-cicd/kustomization.yaml")
+  source = "./src/kustomization.yaml"
+  etag   = filemd5("./src/kustomization.yaml")
 }
 
 resource "aws_s3_object" "gwangju-cicd-requirements" {
   bucket = aws_s3_bucket.gwangju-cicd-app.id
   key    = "/requirements.txt"
-  source = "./src/gwangju-cicd/requirements.txt"
-  etag   = filemd5("./src/gwangju-cicd/requirements.txt")
+  source = "./src/requirements.txt"
+  etag   = filemd5("./src/requirements.txt")
 }
 
 resource "aws_s3_object" "gwangju-cicd-cluster" {
   bucket = aws_s3_bucket.gwangju-cicd-manifest.id
   key    = "/cluster.yaml"
-  source = "./src/gwangju-cicd/manifest/cluster.yaml"
-  etag   = filemd5("./src/gwangju-cicd/manifest/cluster.yaml")
+  source = "./src/manifest/cluster.yaml"
+  etag   = filemd5("./src/manifest/cluster.yaml")
 }
